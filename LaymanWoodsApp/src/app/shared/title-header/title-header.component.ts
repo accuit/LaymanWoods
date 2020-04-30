8999e7a9192e383dd0ad0ef90a5e52c56e0e39e3
Neeraj Singh <accuit@outlook.vom>
2020-04-30 19:28:34 +0530

    Added Video banner on Top

--- LaymanWoodsApp/src/app/shared/title-header/title-header.component.ts
+++ LaymanWoodsApp/src/app/shared/title-header/title-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, AfterViewInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
+import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-title-header',
@@ -7,23 +8,30 @@ import { Component, OnInit, Input, AfterViewInit, ElementRef, ViewChild, Rendere
 })
 export class TitleHeaderComponent implements OnInit, AfterViewInit {
 
-  @Input('type') type: string;
+  @Input('type') type = 'image';
   @Input('bg-image') bgImage: string;
+  @Input('url') url: string;
   @Input('title') title: string;
   @Input('subTitle') subTitle: string;
   @Input('parent') parent: string;
   @Input('parent-path') parentPath: string;
   @Input('current-page') current: string;
   @ViewChild('image', { static: true }) imageEl: ElementRef;
+  loadVideo = false;
 
-  constructor(private readonly el: ElementRef, private readonly renderer: Renderer2) { }
+  constructor( private readonly renderer: Renderer2) { }
 
   ngOnInit() {
     this.bgImage = '' ? 'http://wex.com.pk/images/calculator-banner.png' : this.bgImage;
   }
 
   ngAfterViewInit() {
-    this.renderer.setAttribute(this.imageEl.nativeElement, 'data-background', this.bgImage);
+
+    if (this.type === 'video') {
+      this.loadVideo = true
+    }
+    if (this.imageEl)
+      this.renderer.setAttribute(this.imageEl.nativeElement, 'data-background', this.bgImage);
   }
 
 }
